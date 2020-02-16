c3e37ca680c94d84af8d1827efef2e35047a4338
Filip Golewski <f.golewski@gmail.com>
2020-02-16 21:09:38 +0100

    Source files

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Welcome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Energy.Core.Tilde.WriteLine("~y~Welcome");
+            string name = Energy.Core.Tilde.Input("~g~Enter your name: ", "");
+            if ((name = name.Trim()).Length > 0)
+            {
+                Energy.Core.Tilde.WriteLine($"~w~Hello, ~r~{name}~w~!");
+                Energy.Core.Tilde.WriteLine("~c~Nice to meet you.");
+            }
+            Energy.Core.Tilde.Pause();
+        }
+    }
+}
